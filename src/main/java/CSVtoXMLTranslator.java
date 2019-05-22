2bf7ea8d90c88a410a740dde4bf10496667b9879
VicSz <vicsz>
2019-05-22 12:35:00 -0400

    Refactoring changes.

--- src/main/java/CSVtoXMLTranslator.java
+++ src/main/java/CSVtoXMLTranslator.java
@@ -1,4 +1,4 @@
-import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 
 import java.util.List;
@@ -18,19 +18,27 @@ public class CSVtoXMLTranslator {
         @Override
         public void exitRow(CSVParser.RowContext ctx) {
             stringBuilder.append("<row>");
+
             for(int i = 0; i < headersNames.size(); i++){
-                stringBuilder.append("<" + headersNames.get(i) + ">" + ctx.TEXT(i).getSymbol().getText() + "</" + headersNames.get(i) + ">");
+                stringBuilder.append("<")
+                        .append(headersNames.get(i))
+                        .append(">")
+                        .append(ctx.TEXT(i).getSymbol().getText())
+                        .append("</")
+                        .append(headersNames.get(i))
+                        .append(">");
             }
+
             stringBuilder.append("</row>\n");
         }
 
-        public String getXmlText(){
+        String getXmlText(){
             return "<root>\n" + stringBuilder.toString() + "</root>";
         }
     }
 
     public String map(String csvText){
-        CSVLexer lexer = new CSVLexer(new ANTLRInputStream(csvText));
+        CSVLexer lexer = new CSVLexer(CharStreams.fromString(csvText));
         CSVParser parser = new CSVParser(new CommonTokenStream(lexer));
         XMLFromCSVListener listener = new XMLFromCSVListener();
 
